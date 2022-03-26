55ec6836478f8b6e003069b651ab0bd58d1a078a
Roger <rogerjose81@gmail.com>
2022-03-26 11:40:13 -0500

    docs: improve docs

--- components/header.tsx
+++ components/header.tsx
@@ -15,7 +15,10 @@ export const Header: React.FC = () => {
         </Link>
         <ThemeSwitch />
         <a href="https://github.com/rogerpadilla/uql" title="github" target="_blank" rel="noopener noreferrer">
-          <Image src="/github-logo.svg" width="28" height="28" alt="github" />
+          <Image src="/github-logo.svg" width="20" height="20" alt="github" />
+        </a>
+        <a href="https://www.npmjs.com/package/@uql/core" title="npm" target="_blank" rel="noopener noreferrer">
+          <Image src="/npm-logo.webp" width="28" height="28" alt="npm" />
         </a>
       </nav>
     </header>
