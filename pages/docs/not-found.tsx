452c4cf8cfeee938aadcdf4c08b8e3554a4b6540
Roger <rogerjose81@gmail.com>
2023-01-02 13:10:39 -0500

    feat: add page-not-found

--- pages/docs/not-found.tsx
+++
@@ -1,9 +0,0 @@
-import { Layout } from '../../shared/layout';
-
-const NotFound = () => (
-  <Layout title="Not found" description="Not found">
-    <div>Page not found.</div>
-  </Layout>
-);
-
-export default NotFound;
